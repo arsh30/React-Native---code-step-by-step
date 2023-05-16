3b093a93e56d457b4f1e60f51a1435a7edc1f942
Arshdeep Singh <as3096734@gmail.com>
2023-05-16 14:06:41 +0530

    update

---
+++ reactNativeLearning/App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+export default App;
