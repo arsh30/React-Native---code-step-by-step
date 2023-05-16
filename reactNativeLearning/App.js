38f63f8206a1695d35a56f387bdcbedf1f8c4b3f
Arshdeep Singh <as3096734@gmail.com>
2023-05-16 19:00:07 +0530

    update

--- reactNativeLearning/App.js
+++ reactNativeLearning/App.js
@@ -5,14 +5,49 @@
  * @format
  */
 
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
+const name = 'arsh';
+let age = 19;
+var email = 'abc@test.com';
+
+function fruit() {
+  return 'Apple';
+}
 
 function App() {
   return (
     <View>
-      <Text>Hello world</Text>
+      <Text style={{fontSize: 30}}>Components</Text>
+      {/* use the below component in this */}
+      <User />
+      {/* using the below component */}
+      <User />
+
+      {/* using the 2nd component */}
+      <CompanyData />
     </View>
   );
 }
 
+// function component in same file
+const User = () => {
+  // ARROW fun is js function
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Name: Arsh</Text>
+      <Text style={{fontSize: 30}}>Age: 29</Text>
+      <Text style={{fontSize: 30}}>Email : abc@test.com</Text>
+    </View>
+  );
+};
+
+const CompanyData = () => {
+  // 2nd component
+  return (
+    <View>
+      <Text style={{fontSize: 20}}>Name:XYZ COMPANY</Text>
+      <Text style={{fontSize: 20}}>Area: Delhi</Text>
+    </View>
+  );
+};
 export default App;
